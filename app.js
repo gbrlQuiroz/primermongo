b6ce5071a5b6109f7844fc1379f1aed7e3b3228f
gbrlQuiroz <orion_fire_qog@hotmail.com>
2017-08-10 18:29:36 -0500

    todo ok

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var tabla = require('./routes/tabla');
 
 
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/tabla',tabla);
+
 
 
 // catch 404 and forward to error handler
--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,17 @@ var inject = require('gulp-inject');
 
 
 gulp.task('minify-js', function () {
-    gulp.src('./js/*.js')
-        .pipe(concat('build.js'))
+    gulp.src('./app/js/*.js')
+        .pipe(concat('build.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/www/'));
 });
 
-gulp.task('minify-css',function(){
-    gulp.src('./css/*.css')
-    .pipe(concat('build.css'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./dist/www/'));
+gulp.task('minify-css', function () {
+    gulp.src('./app/css/*.css')
+        .pipe(concat('build.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/www/'));
 });
 
 gulp.task('index', function () {
@@ -24,30 +24,25 @@ gulp.task('index', function () {
         addRootSlash: false,
         ignorePath: 'dist'
     };
-  var target = gulp.src('./index.html');
-  var sources = gulp.src(['./dist/www/*.js', './dist/www/*.css'], {read: false}, {relative: true});
-  return target.pipe(inject(sources, injectOptions))
-    .pipe(gulp.dest('./dist/'));
+    var target = gulp.src('./app/index.html');
+    var sources = gulp.src(['./dist/www/*.js', './dist/www/*.css'], { read: false }, { relative: true });
+    return target.pipe(inject(sources, injectOptions))
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('compilar',['minify-js', 'minify-css']);
-
-gulp.task('copy-img', function() {
-  gulp.src('./img/**')
-    .pipe( gulp.dest('./dist/img') );
+gulp.task('copy-img', function () {
+    gulp.src('./app/img/**')
+        .pipe(gulp.dest('./dist/img'));
 });
 
+gulp.task('compilar', ['minify-js', 'minify-css']);
+gulp.task('compilar2', ['index', 'copy-img']);
 
-/*
-//falla por lo asincrono
-gulp.task('compilar', ['minify-js', 'minify-css'], function () {
-    var injectOptions = {
-        addRootSlash: false,
-        ignorePath: 'dist'
-    };
-  var target = gulp.src('./index.html');
-  var sources = gulp.src(['./dist/www/*.js', './dist/www/*.css'], {read: false}, {relative: true});
-  return target.pipe(inject(sources, injectOptions))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('watch', function() {
+  gulp.watch('./app/js/*.js', ['minify-js']);
+  gulp.watch('./app/css/*.css', ['minify-css']);
+  gulp.watch('./app/*.html', ['index']);
+  gulp.watch('./app/img/**', ['copy-img']);
 });
-*/
+
+gulp.task('default', ['watch']);
