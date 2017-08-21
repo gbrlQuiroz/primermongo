bb12bc9824a2437874f665b0a54df0cbad23be37
gbrlQuiroz <orion_fire_qog@hotmail.com>
2017-08-21 18:43:01 -0500

    todo OK, terminado primer modulo

--- routes/alta.js
+++ routes/alta.js
@@ -8,13 +8,27 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
   var db = req.db;
   var usuarios = db.get('usuarios');
-  usuarios.count({ /*query de MongoDB*/ }).then((cuenta) => {
-if(cuenta>0){
-
-}else{
-  
-}
-  })
+  usuarios.count({
+    nombre: req.body.txtUsuario,
+    clave: req.body.txtClave
+  }).then((cuenta) => {
+    if (cuenta > 0) {
+      res.render('alta', {
+        mensaje: 'Error!: Datos duplicados'
+      });
+    } else {
+      usuarios.insert({
+        clave: req.body.txtClave,
+        usuario: req.body.txtUsuario,
+        nomCompleto: re.body.txtNombreC
+      }).then((error) => {
+        res.render('index');
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  });
+  db.close()
 });
 
 module.exports = router;
